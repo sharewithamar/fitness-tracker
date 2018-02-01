97ce899c20537447994101d1e9d5d19cca410b97
Amar <sharewithamar@gmail.com>
2018-02-01 20:11:52 +0530

    basic nav bar setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core/src/metadata/di';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  onToggle() {}
 }
